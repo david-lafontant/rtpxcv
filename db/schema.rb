0bd084b65785f1c03f50dd91e6cbf5eba7e58b92
David Lafontant <lafontant.david@gmail.com>
2024-07-25 18:45:05 -0400

    Create contents table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_25_223324) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_25_224054) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_25_223324) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "contents", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.bigint "emission_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["emission_id"], name: "index_contents_on_emission_id"
+  end
+
   create_table "emissions", force: :cascade do |t|
     t.string "title"
     t.string "presenter"
@@ -61,4 +70,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_25_223324) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "contents", "emissions"
 end
