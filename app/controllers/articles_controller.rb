b897fa5af8e39262f63650e79d7beaa545e190aa
David Lafontant <lafontant.david@gmail.com>
2024-07-28 22:50:55 -0400

    load and authorize articles resources

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit update destroy]
   before_action :authenticate_user!
+  load_and_authorize_resource
 
   # GET /articles or /articles.json
   def index
