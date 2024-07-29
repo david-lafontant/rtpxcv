1c5a6e5f1c20c5a2e6f309ab51630fc29b5a1fb6
David Lafontant <lafontant.david@gmail.com>
2024-07-28 22:00:03 -0400

    Setup the diferent roles and the default one as user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :trackable 
+
+  enum role: [:user, :writer, :admin]
+
+  after_initialize :set_default_role, if: :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+
 end
