6d1bfc52680ea31eb30109f8e0838a38e8b0eca9
David Lafontant <lafontant.david@gmail.com>
2024-07-29 06:09:17 -0400

    Add foreign key user_id in emissions table

---
+++ db/migrate/20240729100838_add_user_ref_to_emission.rb
@@ -0,0 +1,5 @@
+class AddUserRefToEmission < ActiveRecord::Migration[7.1]
+  def change
+    add_reference :emissions, :user, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_29_100158) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_29_100838) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,6 +80,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_29_100158) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_emissions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -114,4 +116,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_29_100158) do
   add_foreign_key "articles", "users"
   add_foreign_key "contents", "emissions"
   add_foreign_key "contents", "users"
+  add_foreign_key "emissions", "users"
 end
