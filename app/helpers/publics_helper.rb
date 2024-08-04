6f2704b4c6448e7ad6b7a66eea975be85341c908
David Lafontant <lafontant.david@gmail.com>
2024-08-04 10:45:24 -0400

    Create dynamic_url helper to create link adress

--- app/helpers/publics_helper.rb
+++ app/helpers/publics_helper.rb
@@ -2,4 +2,8 @@ module PublicsHelper
     def published_article(article)
      "/published-artcles/#{article.slug}"
     end
+
+    def dynamic_url(model, field)
+      "/published-#{model}/#{field}"
+    end
 end
