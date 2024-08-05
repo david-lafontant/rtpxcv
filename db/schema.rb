c739154453b7b489ca54b3f693c89f4866fe71e4
David Lafontant <lafontant.david@gmail.com>
2024-08-04 20:01:22 -0400

    Add published column in emissions table with default value as false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_29_100838) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_04_235645) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -81,6 +81,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_29_100838) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.boolean "published", default: false, null: false
     t.index ["user_id"], name: "index_emissions_on_user_id"
   end
 
