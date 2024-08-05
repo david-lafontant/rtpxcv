a953cf2f9b50acba4cc81e52eaeee4eb37e7b732
David Lafontant <lafontant.david@gmail.com>
2024-08-04 20:04:11 -0400

    Add published column to contents with default value as false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_04_235645) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_05_000024) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,6 +70,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_04_235645) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.boolean "published", default: false, null: false
     t.index ["emission_id"], name: "index_contents_on_emission_id"
     t.index ["user_id"], name: "index_contents_on_user_id"
   end
